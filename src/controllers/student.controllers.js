0644e4b8b375a82873a76cef9e484da09dbf5d77
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2024-09-16 10:57:32 +0530

    added jwt tokens generation while registering

--- src/controllers/student.controllers.js
+++ src/controllers/student.controllers.js
@@ -1,25 +1,25 @@
 
 const Student = require('../models/student.js');
-const {generateAccessToken,generateRefreshToken}=require('../utils/generateTokens.js')
-const jwt=require('jsonwebtoken')
-const generateAccessAndRefreshToken= async (studentId)=>{
-  try {
-     const student= await Student.findById(studentId)
-     const accessToken= generateAccessToken(student);
-     const refreshToken=  generateRefreshToken(student);
-     
-     student.refreshToken=refreshToken;
-      await student.save({validateBeforeSave:false});
-      return {accessToken,refreshToken};
-  } catch (error) {
-    res.status(400).send('Some thing went wrong while generating tokens')
-  }
+const { generateAccessToken, generateRefreshToken } = require('../utils/generateTokens.js')
+const jwt = require('jsonwebtoken')
+const generateAccessAndRefreshToken = async (studentId) => {
+    try {
+        const student = await Student.findById(studentId)
+        const accessToken = generateAccessToken(student);
+        const refreshToken = generateRefreshToken(student);
+
+        student.refreshToken = refreshToken;
+        await student.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
+        res.status(400).send('Some thing went wrong while generating tokens')
+    }
 
 }
 
-const studentRegister= async (req,res)=>{
+const studentRegister = async (req, res) => {
     try {
-       
+
         const firstname = req.body.firstname;
         const lastname = req.body.lastname;
         const rollnum = req.body.rollnum;
@@ -29,70 +29,76 @@ const studentRegister= async (req,res)=>{
         const password = req.body.password;
         const confirmPassword = req.body.confirmpassword;
         const studentClass = req.body.studentclass;
-        if(password===confirmPassword){
+        if (password === confirmPassword) {
 
-            const studentExists= await Student.findOne({rollnum,schoolname:schoolname.toLowerCase(),class:studentClass});
-            if(!studentExists){
-             const studentData= await Student.create({
-                firstname:firstname.toLowerCase(),
-                lastname:lastname.toLowerCase(),
-                rollnum,
-                contactnum:phonenumber,
-                schoolname:schoolname.toLowerCase(),
-                gender,
-                password,
-                class:studentClass
-             });
-             res.render('student_index',{studentData});
+            const studentExists = await Student.findOne({ rollnum, schoolname: schoolname.toLowerCase(), class: studentClass });
+            if (!studentExists) {
+                const studentData = await Student.create({
+                    firstname: firstname.toLowerCase(),
+                    lastname: lastname.toLowerCase(),
+                    rollnum,
+                    contactnum: phonenumber,
+                    schoolname: schoolname.toLowerCase(),
+                    gender,
+                    password,
+                    class: studentClass
+                });
+                const { accessToken, refreshToken } = await generateAccessAndRefreshToken(studentData._id)
+
+                const options = {
+                    httpOnly: true,
+                    secure: true
+                }
+                return res.status(200).cookie("accessToken", accessToken, options).cookie('refreshToken', refreshToken, options).redirect('/');
             }
-            else{
-                res.redirect('/student-login');
+            else {
+                return res.redirect('/student-login');
             }
         }
-        else{
-            res.send("Invalid Credential!!")
+        else {
+            return res.send("Invalid Credential!!")
         }
 
-         
+
 
     } catch (error) {
-        res.send(`Error occured:${error} `)
+        return res.send(`Error occured:${error} `)
     }
 };
 
 
 
 
-const studentLogin= async (req,res)=>{
+const studentLogin = async (req, res) => {
     try {
 
         const studentClass = req.body.studentclass
         const rollnum = req.body.rollnum
         const password = req.body.password
-        const studentData = await Student.findOne({ rollnum: rollnum, class: studentClass, password})
-        
-        if(!studentData){
+        const studentData = await Student.findOne({ rollnum: rollnum, class: studentClass, password })
+
+        if (!studentData) {
             res.send('Invalid Credentials')
         }
-        const {accessToken,refreshToken}=await generateAccessAndRefreshToken(studentData._id)
-        
-        const options={
-            httpOnly:true,
-            secure:true
+        const { accessToken, refreshToken } = await generateAccessAndRefreshToken(studentData._id)
+
+        const options = {
+            httpOnly: true,
+            secure: true
         }
-        res.status(200).cookie("accessToken",accessToken,options).cookie('refreshToken',refreshToken,options).redirect('/');
-     
+        res.status(200).cookie("accessToken", accessToken, options).cookie('refreshToken', refreshToken, options).redirect('/');
+
     } catch (error) {
         console.error(error)
         res.status(400).send(`Error:${error} `)
     }
 
 };
-const studentIndex=async (req,res)=>{
+const studentIndex = async (req, res) => {
     try {
-        
-        const studentInfo= await Student.findById(req.user._id).select('-password -refreshToken');
-        res.status(201).render('student_index',{studentInfo})
+
+        const studentInfo = await Student.findById(req.user._id).select('-password -refreshToken');
+        res.status(201).render('student_index', { studentInfo })
 
     } catch (error) {
         throw error;
@@ -100,52 +106,51 @@ const studentIndex=async (req,res)=>{
 
 }
 
-const studentLogout= async (req,res)=>{
+const studentLogout = async (req, res) => {
     try {
-      
-        await Student.findByIdAndUpdate(req.user._id,{$set:{refreshToken:undefined}},{new:true}).select('-password')
-        const options={
-            httpOnly:true,
-            secure:true
+
+        await Student.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } }, { new: true }).select('-password')
+        const options = {
+            httpOnly: true,
+            secure: true
         }
-      return  res.status(201).clearCookie("accessToken",options).clearCookie("refreshToken",options).redirect('/student-login')
+        return res.status(201).clearCookie("accessToken", options).clearCookie("refreshToken", options).redirect('/student-login')
     } catch (error) {
         throw error;
     }
 }
 
-const refreshAccessToken=async (req,res)=>{
-    const oldRefreshToken=req.cookies?.refreshToken
-    
-   try {
-     if(!oldRefreshToken){
-         res.status(401).redirect('/student-login')
+const refreshAccessToken = async (req, res) => {
+    const oldRefreshToken = req.cookies?.refreshToken
+
+    try {
+        if (!oldRefreshToken) {
+            res.status(401).redirect('/student-login')
         }
-        
-        const verifiedOldToken= jwt.verify(oldRefreshToken,'mynameisdev')
-     const studentRefresh= await Student.findById(verifiedOldToken?._id)
-   
-     if(!studentRefresh){
-         res.status(401).send('Invalid Refresh Token!')
-     }
- 
-     if(oldRefreshToken!==studentRefresh?.refreshToken)
-     {
-         res.status(404).redirect('/student-login')
-     }
-     const {accessToken,refreshToken}= await generateAccessAndRefreshToken(studentRefresh._id)
-     
-     const options={
-         httpOnly:true,
-         secure:true
-     }
-     res.status(200).cookie('accessToken',accessToken,options).cookie('refreshToken',refreshToken,options).redirect('/')
-   } catch (error) {
+
+        const verifiedOldToken = jwt.verify(oldRefreshToken, 'mynameisdev')
+        const studentRefresh = await Student.findById(verifiedOldToken?._id)
+
+        if (!studentRefresh) {
+            res.status(401).send('Invalid Refresh Token!')
+        }
+
+        if (oldRefreshToken !== studentRefresh?.refreshToken) {
+            res.status(404).redirect('/student-login')
+        }
+        const { accessToken, refreshToken } = await generateAccessAndRefreshToken(studentRefresh._id)
+
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+        res.status(200).cookie('accessToken', accessToken, options).cookie('refreshToken', refreshToken, options).redirect('/')
+    } catch (error) {
         res.status(400).send(error)
-   }
+    }
 
 }
-module.exports={
+module.exports = {
     studentRegister,
     studentLogin,
     studentIndex,
--- src/middlewares/student.auth.js
+++ src/middlewares/student.auth.js
@@ -5,7 +5,7 @@ const verifyStudent= async (req,res,next)=>{
     const token=req.cookies?.accessToken;
    
     if(!token){
-         res.status(401).redirect('/student-login')
+        return res.status(401).redirect('/student-login')
      }
  try {
        const verifiedToken= jwt.verify(token,'mynameisdev')
@@ -13,16 +13,16 @@ const verifyStudent= async (req,res,next)=>{
       
        const student= await Student.findById(verifiedToken?._id).select('-password -refreshToken')
        if(!student){
-           res.status(401).send("Invalid Token!!")
+          return res.status(401).send("Invalid Token!!")
        }
        req.user=student;
        next();
  } catch (error) {
-    console.log(error)
+    
     if(error.name==='TokenExpiredError'){
         return res.redirect('/student/refreshtoken')
     }
-    res.status(400).send('Internal Server Error')
+   return  res.status(400).send('Internal Server Error')
     
  }
 }
