c06b926ccb2ef649308e06d140c29b3c15a71bbc
Devpratap-Tomar <devpratap.tomar2005@gmail.com>
2024-10-04 22:48:58 +0530

    insert new table row when attendence is marked

--- public/scripts/mark_attendence.js
+++ public/scripts/mark_attendence.js
@@ -21,4 +21,18 @@ const updatedAttendence= await fetch('/teacher/mark-attendence',{
 })
 const updatedStudent= await updatedAttendence.json()
 console.log(updatedStudent)
+
+const attendenceTable=document.querySelector('.updated-attendence-table').firstElementChild
+const row=document.createElement('tr')
+row.innerHTML=`<td>${updatedStudent.firstname}</td>
+<td>${updatedStudent.lastname}</td>
+<td>${updatedStudent.rollnum}</td>
+<td>${updatedStudent.gender}</td>
+<td>${updatedStudent.class}</td>
+<td>${updatedStudent.lastmarked}</td>
+<td>${attendenceStatus}</td>`
+attendenceTable.appendChild(row)
+if(document.querySelector('.updated-attendence-table').classList.contains('disp-none')){
+    document.querySelector('.updated-attendence-table').classList.remove('disp-none')
+}
 })
